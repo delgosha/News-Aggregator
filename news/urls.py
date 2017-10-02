de1d63058f5395000cc3ebd22a3ac9222d6c95a3
delgosha <msdelgosha@gmail.com>
2017-10-02 17:13:41 +0330

    adding feedparser

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.articles_list, name = 'articles_list')
+	url(r'^$', views.articles_list, name = 'articles_list'),
+	url(r'^feeds/$', views.feeds_list, name = 'feeds_list'),
+	url(r'^feeds/add/$', views.add_feed, name = 'add_feed')
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
+from .models import Article, Feed
+from .forms import FeedForm
+from django.shortcuts import redirect
+
+import feedparser
+from datetime import datetime
 
 # Create your views here.
 def articles_list(request):
-	return render(request, 'news/articles_list.html', {})
+	articles = Article.objects.all()
+	return render(request, 'news/articles_list.html', {'articles': articles})
+
+def feeds_list(request):
+	feeds = Feed.objects.all()
+	return render(request, 'news/feeds_list.html', {'feeds': feeds})
+
+def add_feed(request):
+	if request.method == "POST":
+		form = FeedForm(request.POST)
+		if form.is_valid():
+			feed = form.save(commit = False)
+			feedData = feedparser.parse(feed.url)
+			# handling fields
+			feed.title = feedData.feed.title
+			feed.save()
+
+			for entry in feedData.entries:
+				article = Article()
+				article.title = entry.title
+				article.url = entry.link
+				article.description = entry.description
+				d = datetime(*(entry.published_parsed[0:6]))
+				datestring = d.strftime('%Y-%m-%d %H:%M:%S')
+				article.publication_date = datestring
+				article.feed = feed
+				article.save()
+
+
+			return redirect('feeds_list')
+	else:
+		form = FeedForm
+	return render(request, 'news/add_feed.html', {'form': form})
